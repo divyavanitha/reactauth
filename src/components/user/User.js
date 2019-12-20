b381d3b23133b865c9842bc2e7984834c55d012d
divya <divyak@appoets.com>
2019-12-20 07:56:10 +0530

    modal added

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -8,6 +8,12 @@ class Home extends Component {
     componentDidMount(){
         this.props.getUsers();
     }
+
+    handleDelete = (id) => {
+      var users = this.props.users.filter(user => user._id !== id);
+      this.setState({users});
+    }
+
     render() {
         return ( <React.Fragment>
             
@@ -26,12 +32,32 @@ class Home extends Component {
             <tr key={user._id}>
               <td>{user.name}</td>
               <td>{user.email}</td>
-              <td>Delete</td>
+              <td className="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" data-value={user._id}>Delete</td>
             </tr>
           ))}
         </tbody>
         </table>
         
+
+
+        <div className="modal fade" id="myModal" role="dialog">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal">&times;</button>
+              <h4 className="modal-title">Modal Header</h4>
+            </div>
+            <div className="modal-body">
+              <p>Are you sure want to delete</p>
+            </div>
+            <div className="modal-footer">
+            <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={() => this.handleDelete()}>Yes</button>
+              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+          
+        </div>
+      </div>
         <Footer/></React.Fragment> );
     }
 }
